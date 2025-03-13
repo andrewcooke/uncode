7ecbd72fad598abbb78ef231e11bf64f7c1b0ce3
andrew cooke <andrew@acooke.org>
2025-03-13 02:14:27 -0300

    small tweaks

--- py/anneal.py
+++ py/anneal.py
@@ -13,28 +13,30 @@ def anneal(code, fmt, ngrams, words, neighbours, steps, heat, gamma, weight, eve
             max_temp = max(max_temp, abs(base - guess.swap().score(ngrams, words, weight)))
         return max_temp * heat
 
-    def dump(old_score, old_guess, temp, i):
-        log.info(f'\nscore: {old_score:4.2f}  temp: {temp:5.3f}  countdown: {i}/{steps}')
+    def dump(score, guess, temp, i, swaps):
+        log.info(f'score: {score:4.2f}  temp: {temp:5.3f}  countdown: {i}/{steps}  swaps: {swaps}')
+        log.debug(f'{ngrams.alphabet}')
         log.info(f'{fmt(code)}')
-        log.info(f'{old_guess}')
+        log.warning(f'{guess}\n')
 
     old_guess = Guess(code, ngrams, neighbours)
     old_score = old_guess.score(ngrams, words, weight)
 
     max_temp = estimate_max_temp(old_guess)
 
-    count_swap = 0
+    swaps = 0
     for i in range(steps, 0, -1):
         temp = max_temp * pow(i / steps, gamma)
         new_guess = old_guess.swap()
         new_score = new_guess.score(ngrams, words, weight)
         # if new_score > old_score this is -ve and always selected
         if temp > (old_score - new_score):
-            count_swap += 1
+            swaps += 1
             old_guess = new_guess
             old_score = new_score
         if i % every == 0:
-            dump(old_score, old_guess, temp, i)
-    dump(old_score, old_guess, 0.0, 0)
+            dump(old_score, old_guess, temp, i, swaps)
+    dump(old_score, old_guess, 0.0, 0, swaps)
+    log.error(f'{old_guess}')
 
 
